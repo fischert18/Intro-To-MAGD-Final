c21f463dd8200d8de245b8158340d8f689023e07
fischert18 <tylerfischer4789@gmail.com>
2026-05-07 14:48:40 -0500

    pHit works!
    accidentally commited other edits from today to main

--- P5JS Blackjack/sketch.js
+++ P5JS Blackjack/sketch.js
@@ -2,31 +2,52 @@ let deck = [];
 let suit = 'unknown';
 let rank = 0;
 
+class Card {
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+  
+  printDeck() {
+    print(`${this.rank} ${this.suit}`)
+  }
+  
+  pHit() {
+  drawCard(50, 100, this.rank, this.suit);
+  }
+  
+}
+
+function deckCreate() {
+   for (let i = 1; i <= 52; i++) {
+    cardCheck(i);
+    let card = new Card(rank, suit);
+    deck.push(card);
+  }
+}
+
+
 function setup() {
   colorMode (RGB, 255, 255, 255, 1);
   createCanvas(500, 500);
-  for (let i = 0; i < 52; i++) {
-    deck.push(i);
-  }
+  deckCreate();
   noLoop();
 }
 
-function card(x, y, rank, suit) {
+//creates card visual
+function drawCard(x, y, rank, suit) {
   push();
   translate(x, y);
-  rect(0, 0, 80, 120)
-  translate(0 , 30)
+  rect(0, 0, 80, 120);
+  translate(0 , 30);
   text(rank, 40, 40);
+  translate(0, 10);
+  text(suit, 40, 40);
   pop();
 }
 
-function pHit() {
-  x = random(deck);
-  card(50, 100, x);
-  suitCheck(x);
-  print(suit);
-}
-
+//gives card approrpriate suit based on value
+//1-13 heart, 14-26 diamond, 27-39 club, 40-52 spade
 function suitCheck(card){
   if (card >= 0 && card <= 13) {
     suit = 'heart'
@@ -42,8 +63,21 @@ function suitCheck(card){
   }
 }
 
-function rankCheck(value) {
-  
+//gives card appropriate rank based on suit
+function cardCheck(value) {
+  suitCheck(value);
+  if (suit == 'heart') {
+    rank = value;
+  }
+  else if (suit == 'diamond') {
+    rank = value - 13;
+  }
+  else if (suit == 'club') {
+    rank = value - 26;
+  }
+  else {
+    rank = value - 39;
+  }
 }
 
 function pStand() {
@@ -60,5 +94,9 @@ function dStand() {
 
 function draw() {
   background(30, 145, 50);
-  pHit();
+  hitCard = random(deck)
+  for (let card of deck){
+    card.printDeck();
+  }
+  hitCard.pHit();
 }
